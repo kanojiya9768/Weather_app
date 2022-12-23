2a05327a5ad70016a93552c40b674647b21f3f1e
Vishal Brijesh kanojiya <90042549+kanojiya9768@users.noreply.github.com>
2022-12-23 20:59:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// have used universal weather app api 
+document.getElementById('search').addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch_api();
+
+  document.getElementById('searched_text').innerHTML = '';
+})
+
+
+
+
+//fetch api here 
+let fetch_api = async () => {
+  let city_name = document.getElementById('searched_text').value;
+
+  let url = `https://api.weatherapi.com/v1/current.json?key=cb80e1fac84c43beb82105100211509&q=${city_name}&aqi=no`;
+
+  let res = await fetch(url);
+  let data = await res.json();
+
+  //call show data function to display the data on screen
+  show_data(data);
+
+  //print response on console to make sure that response is comming succefully
+  console.log(data);
+}
+
+
+
+
+
+
+//show data method is written here 
+let show_data = (newdata) => {
+
+  let time = new Date(newdata.location.localtime).toDateString();
+  document.getElementById('cards').style.display = 'flex';
+  
+
+  let data = `
+   <div id='card'>
+      <div id='places'>
+        <p>${newdata.location.name}</p>
+        <p>${newdata.location.region}</p>
+        <p>${newdata.location.country}</p>
+      </div>
+      
+      <p>${time}</p>
+      <img src=${newdata.current.condition.icon} alt='icon'>
+      
+        <p>Temprature in Cencius : <b>${newdata.current.temp_c} °C</b></p>
+        <p>Temprature in Fahrenheit : <b>${newdata.current.temp_f} °F</b></p>
+    </div>`
+
+  document.getElementById('cards').innerHTML = data;
+
+}
