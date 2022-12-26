a484894782f71a670cab94d5f39f161ea661831a
Vishal Brijesh kanojiya <90042549+kanojiya9768@users.noreply.github.com>
2022-12-26 12:22:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 // have used universal weather app api 
-
-//onform submission
 document.getElementById('search').addEventListener('submit', (e) => {
   e.preventDefault();
   fetch_api();
 
-  document.getElementById('searched_text').innerHTML = '';
-})
-
-
-
-//onlick of search button
-document.getElementById('search_city').addEventListener('click', (e) => {
-  e.preventDefault();
-  fetch_api();
-
-  document.getElementById('searched_text').innerHTML = '';
+  document.getElementById('searched_text').innerText = '';
 })
 
 
 
 
 //fetch api here 
-let fetch_api = async () => {
-  let city_name = document.getElementById('searched_text').value;
+let fetch_api = () => {
 
+  let city_name = document.getElementById('searched_text').value;
   let url = `https://api.weatherapi.com/v1/current.json?key=cb80e1fac84c43beb82105100211509&q=${city_name}&aqi=no`;
 
-  let res = await fetch(url);
-  let data = await res.json();
+  fetch(url).then((res)=>{
+    return res.json();
+  }).then((data)=>{
+    //call show data function to display the data on screen
+    show_data(data);
 
-  //call show data function to display the data on screen
-  show_data(data);
-
-  //print response on console to make sure that response is comming succefully
-  console.log(data);
+    //print response on console to make sure that response is comming succefully
+   console.log(data);
+  }).catch(err =>{
+    alert("Enter valid city , State or Country Name.!")
+  })
+  
 }
 
 
@@ -46,7 +38,9 @@ let fetch_api = async () => {
 let show_data = (newdata) => {
 
   let time = new Date(newdata.location.localtime).toDateString();
-  document.getElementById('cards').style.display = 'flex';
+  document.getElementById('cards').style.display = 'flex'; 
+
+  let localtime = new Date(newdata.location.localtime).toLocaleTimeString();
   
 
   let data = `
@@ -58,6 +52,7 @@ let show_data = (newdata) => {
       </div>
       
       <p>${time}</p>
+      <p>${localtime}</p>
       <img src=${newdata.current.condition.icon} alt='icon'>
       
         <p>Temprature in Cencius : <b>${newdata.current.temp_c} °C</b></p>
